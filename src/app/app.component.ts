c30cd10a324dbc2498016578a7818ebb11fe1106
jferco <jferco94@gmail.com>
2018-09-23 13:37:45 -0600

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'catalogoMongo';
+
+  items:any[]=[
+    {"title":"iPad", "img": "ipad.jpeg", "description": "Tableta Apple iPad"},
+    {"title":"Blusa", "img": "blusa.jpg", "description": "Blusa Azul"},
+    {"title":"Zapatos", "img": "zapatos.jpeg", "description": "Zapatos Vino"},
+    {"title":"Bolso", "img": "bolso.jpg", "description": "Bolso para laptop"},
+    {"title":"Laptop", "img": "laptop.jpg", "description": "Laptop Razer Blade"}
+  ];
+
+  constructor (){
+  }
+
+
 }
